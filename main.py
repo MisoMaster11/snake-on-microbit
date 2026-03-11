b079bb8bdfae3f0a4f134802e83265ccb225e1ed
MisoMaster11 <161627664+MisoMaster11@users.noreply.github.com>
2026-03-11 14:11:59 +0100

    Added new features

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ snake_brightness = 100
 apple_brightness = 255
 
 direction = (1, 0)
-snake = [(0, 2), (1, 2), (2, 2)]
+snake = [(1, 2), (2, 2)]
 apple = (0, 0)
 is_game_over = False
+can_turn = True
+can_restart = False
+
+def reset():
+    global direction, snake, apple, is_game_over, can_turn, can_restart
+    direction = (1, 0)
+    snake = [(1, 2), (2, 2)]
+    apple = (0, 0)
+    is_game_over = False
+    can_turn = True
+    led.stop_animation()
+    basic.clear_screen()
+    plot_snake()
+    spawn_apple()
+    can_restart = False
 
 def spawn_apple():
     global apple
@@ -34,28 +49,43 @@ plot_snake()
 spawn_apple()
 
 def on_button_pressed_a(): #dolava
-    global direction
-    direction = (direction[1], -direction[0])
+    global direction, can_turn
+    if can_turn == True and not is_game_over:
+        direction = (direction[1], -direction[0])
+        can_turn = False
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b(): #doprava
-    global direction
-    direction = (-direction[1], direction[0])
+    global direction, can_turn
+    if can_turn == True and not is_game_over:
+        direction = (-direction[1], direction[0])
+        can_turn = False
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def game_over():
-    global is_game_over
+def on_button_pressed_ab():
+    if can_restart:
+        reset()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def game_over(icon):
+    global is_game_over, can_turn, can_restart
     is_game_over = True
+    can_turn = False
     for i in range(3):
         basic.clear_screen()
         basic.pause(200)
         plot_snake()
         basic.pause(200)
     basic.clear_screen()
-    basic.show_icon(IconNames.SKULL)
+    basic.show_icon(icon)
+    basic.pause(700)
+    basic.clear_screen()
+    basic.pause(300)
+    can_restart = True
+    basic.show_number(len(snake))
 
 def on_every_interval():
-    global snake
+    global snake, can_turn
     if is_game_over == True:
         return
     last_index = len(snake) - 1
@@ -63,21 +93,26 @@ def on_every_interval():
     new_head_y = snake[last_index][1] + direction[1]
     
     if new_head_x > 4 or new_head_x < 0 or new_head_y > 4 or new_head_y < 0:
-        game_over()
+        game_over(IconNames.SKULL)
         return
     
-    for segment in snake:
-        if segment[0] == new_head_x and segment[1] == new_head_y:
-            game_over()
-            return
-
+    snake.append((new_head_x, new_head_y))
     if new_head_x == apple[0] and new_head_y == apple[1]:
         spawn_apple()
     else:
         led.unplot(snake[0][0], snake[0][1])
         snake.pop(0)
 
-    snake.append((new_head_x, new_head_y))
+    for segment in snake[:-1]:
+        if segment[0] == new_head_x and segment[1] == new_head_y:
+            game_over(IconNames.SKULL)
+            return
+
     led.plot_brightness(new_head_x, new_head_y, snake_brightness)
+    can_turn = True
+
+    if len(snake)>=25:
+        game_over(IconNames.DIAMOND)
+
     
 loops.every_interval(500, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,9 +1,25 @@
 let snake_brightness = 100
 let apple_brightness = 255
 let direction = [1, 0]
-let snake = [[0, 2], [1, 2], [2, 2]]
+let snake = [[1, 2], [2, 2]]
 let apple = [0, 0]
 let is_game_over = false
+let can_turn = true
+let can_restart = false
+function reset() {
+    
+    direction = [1, 0]
+    snake = [[1, 2], [2, 2]]
+    apple = [0, 0]
+    is_game_over = false
+    can_turn = true
+    led.stopAnimation()
+    basic.clearScreen()
+    plot_snake()
+    spawn_apple()
+    can_restart = false
+}
+
 function spawn_apple() {
     let new_x: number;
     let new_y: number;
@@ -42,16 +58,31 @@ spawn_apple()
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     // dolava
     
-    direction = [direction[1], -direction[0]]
+    if (can_turn == true && !is_game_over) {
+        direction = [direction[1], -direction[0]]
+        can_turn = false
+    }
+    
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     // doprava
     
-    direction = [-direction[1], direction[0]]
+    if (can_turn == true && !is_game_over) {
+        direction = [-direction[1], direction[0]]
+        can_turn = false
+    }
+    
 })
-function game_over() {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    if (can_restart) {
+        reset()
+    }
+    
+})
+function game_over(icon: number) {
     
     is_game_over = true
+    can_turn = false
     for (let i = 0; i < 3; i++) {
         basic.clearScreen()
         basic.pause(200)
@@ -59,7 +90,12 @@ function game_over() {
         basic.pause(200)
     }
     basic.clearScreen()
-    basic.showIcon(IconNames.Skull)
+    basic.showIcon(icon)
+    basic.pause(700)
+    basic.clearScreen()
+    basic.pause(300)
+    can_restart = true
+    basic.showNumber(snake.length)
 }
 
 loops.everyInterval(500, function on_every_interval() {
@@ -72,17 +108,11 @@ loops.everyInterval(500, function on_every_interval() {
     let new_head_x = snake[last_index][0] + direction[0]
     let new_head_y = snake[last_index][1] + direction[1]
     if (new_head_x > 4 || new_head_x < 0 || new_head_y > 4 || new_head_y < 0) {
-        game_over()
+        game_over(IconNames.Skull)
         return
     }
     
-    for (let segment of snake) {
-        if (segment[0] == new_head_x && segment[1] == new_head_y) {
-            game_over()
-            return
-        }
-        
-    }
+    snake.push([new_head_x, new_head_y])
     if (new_head_x == apple[0] && new_head_y == apple[1]) {
         spawn_apple()
     } else {
@@ -90,6 +120,17 @@ loops.everyInterval(500, function on_every_interval() {
         _py.py_array_pop(snake, 0)
     }
     
-    snake.push([new_head_x, new_head_y])
+    for (let segment of snake.slice(0, -1)) {
+        if (segment[0] == new_head_x && segment[1] == new_head_y) {
+            game_over(IconNames.Skull)
+            return
+        }
+        
+    }
     led.plotBrightness(new_head_x, new_head_y, snake_brightness)
+    can_turn = true
+    if (snake.length >= 25) {
+        game_over(IconNames.Diamond)
+    }
+    
 })
